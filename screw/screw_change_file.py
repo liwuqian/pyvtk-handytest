8126f626ddc1c222a09e1b73d831029716d0de43
JackieLi <wq09680698@163.com>
2023-08-25 15:37:26 +0800

    add change stl file to update actor

---
+++ screw/screw_change_file.py
@@ -0,0 +1,55 @@
+import vtkmodules.all as vtk
+import time
+
+# Create a renderer
+renderer = vtk.vtkRenderer()
+
+# Create a render window
+render_window = vtk.vtkRenderWindow()
+render_window.AddRenderer(renderer)
+
+# Create a render window interactor
+render_window_interactor = vtk.vtkRenderWindowInteractor()
+render_window_interactor.SetRenderWindow(render_window)
+
+# Create a STL reader
+stl_reader = vtk.vtkSTLReader()
+stl_reader.SetFileName("data/37125550-01.stl")
+stl_reader.Update()
+
+# Create a mapper
+mapper = vtk.vtkPolyDataMapper()
+mapper.SetInputConnection(stl_reader.GetOutputPort())
+
+# Create an actor
+actor = vtk.vtkActor()
+actor.SetMapper(mapper)
+actor.GetProperty().SetColor(0.5, 0.8, 0)
+actor.GetProperty().SetOpacity(0.6)
+actor.GetProperty().LightingOff()
+
+# Add the actor to the renderer
+renderer.AddActor(actor)
+
+axes = vtk.vtkAxesActor()
+axes.SetTotalLength(60, 80, 60)
+renderer.AddActor(axes)
+
+# Set background color
+renderer.SetBackground(0.1, 0.1, 0.1)
+
+# Reset the camera to show the object
+renderer.ResetCamera()
+
+# Start rendering
+render_window.Render()
+
+time.sleep(2)
+
+# update reader
+stl_reader.SetFileName("data/screw_general.stl")
+stl_reader.Update()
+render_window.Render()
+
+# Start the interaction
+render_window_interactor.Start()
